719b2335fac94b03701ecb1680d4f918f9e86830
yeyuxin <yuxin.ye@take2.health>
2023-03-06 16:23:27 +0800

    no message

--- gatsby-node.js
+++
@@ -1,8 +0,0 @@
-exports.createPages = async ({ graphql, actions }) => {
-  const { createRedirect } = actions
-  createRedirect({
-    fromPath: `/404/`,
-    toPath: `/404`,
-    statusCode: 200,
-  })
-}
