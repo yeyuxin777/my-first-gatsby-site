fe45fcba2e9cb3cbd4e5e64a142c2565e823debd
yeyuxin <yuxin.ye@take2.health>
2023-03-07 11:32:27 +0800

    build报错，gatsby-plugin-google-gtag 插件配置报错

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,21 @@ module.exports = {
     title: `My Gatsby Site`,
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: ["gatsby-plugin-sass", "gatsby-plugin-google-gtag"]
+  plugins: [
+    "gatsby-plugin-sass", 
+    {
+      resolve: "gatsby-plugin-google-gtag",
+      options: {
+        trackingIds: [process.env.GATSBY_GA_MEASUREMENT_ID],
+        gtagConfig: {
+          optimize_id: [process.env.GATSBY_GTM_ID],
+          anonymize_ip: true,
+          cookie_expires: 0,
+        },
+        pluginConfig: {
+          head: true,
+        },
+      },
+    },
+  ]
 };
