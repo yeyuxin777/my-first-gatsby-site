bde65e15d1a7b70e27c93e89d39371d2c2f77fe7
yeyuxin <yuxin.ye@take2.health>
2023-03-20 16:42:54 +0800

    no message

--- src/html.js
+++ src/html.js
@@ -8,7 +8,7 @@ export default function HTML(props) {
         <meta charSet='utf-8' />
         <meta httpEquiv='x-ua-compatible' content='ie=edge' />
         <meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no' />
-        <script
+        {/* <script
           dangerouslySetInnerHTML={{
             __html: `
             var _gaq = _gaq || []
@@ -27,7 +27,7 @@ export default function HTML(props) {
             })()
             `,
           }}
-        />
+        /> */}
         {/* Google Tag Manager */}
         <script
           dangerouslySetInnerHTML={{
