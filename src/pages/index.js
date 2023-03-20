e8cfd59e5b7d640b00a489a90037638656598b9e
yeyuxin <yuxin.ye@take2.health>
2023-03-20 17:07:33 +0800

    no message

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,10 @@
 import * as React from "react"
 import Layout from '../components/layout'
-import { StaticImage } from 'gatsby-plugin-image'
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial.</p>
-      <StaticImage
-        alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
-        src={`https://image.baidu.com/search/albumsdetail?tn=albumsdetail&word=渐变风格插画&fr=albumslist&album_tab=设计素材&album_id=409&rn=30`}
-      />
     </Layout>
   )
 }
