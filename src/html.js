64439650990ef59e9b1f2b0d67cf9effd6376166
yeyuxin <yuxin.ye@take2.health>
2023-03-21 10:08:27 +0800

    埋点

--- src/html.js
+++ src/html.js
@@ -32,34 +32,32 @@ export default function HTML(props) {
         <script
           dangerouslySetInnerHTML={{
             __html: `
-            (function (w, d, s, l, i) {
-              w[l] = w[l] || []
-              w[l].push({
-                'gtm.start': new Date().getTime(),
-                event: 'gtm.js',
-              })
-              var f = d.getElementsByTagName(s)[0],
-                j = d.createElement(s),
-                dl = l != 'dataLayer' ? '&l=' + l : ''
-              j.async = true
-              j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl
-              f.parentNode.insertBefore(j, f)
-            })(window, document, 'script', 'dataLayer', 'GTM-KFMV35D');
-              `,
+              (
+                function(w, d, s, l, i){ 
+                  w[l] = w[l] || [];
+                  w[l].push({
+                    'gtm.start': new Date().getTime(),
+                    event: 'gtm.js'
+                  });
+                var f = d.getElementsByTagName(s)[0], j = d.createElement(s), dl=l!='dataLayer'?'&l='+l:''; j.async=true;
+                j.src= 'https://www.googletagmanager.com/gtm.js?id='+i+dl;
+                f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-W3KSCDK')
+            `,
           }}
         />
         {/* End Google Tag Manager */}
 
         {/* Global site tag (gtag.js) - Google Ads: 339029822 */}
-        <script async src='https://www.googletagmanager.com/gtag/js?id=G-CMGDHHNWH0'></script>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-5JP83J9LMG"></script>
         <script
           dangerouslySetInnerHTML={{
             __html: `
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-           
-            gtag('config', 'G-CMGDHHNWH0');
+  
+            gtag('config', 'G-5JP83J9LMG');
             `,
           }}
         />
@@ -143,15 +141,12 @@ export default function HTML(props) {
         /> */}
         {/* Google Tag Manager (noscript) */}
         <noscript>
-          <iframe
-            src='https://www.googletagmanager.com/ns.html?id=GTM-KFMV35D'
-            height='0'
-            width='0'
-            style={{
-              display: 'none',
-              visibility: 'hidden',
-            }}
-          ></iframe>
+          <iframe 
+            src="https://www.googletagmanager.com/ns.html?id=GTM-W3KSCDK"
+            height="0" 
+            width="0" 
+            style="display:none;visibility:hidden">
+          </iframe>
         </noscript>
         {props.preBodyComponents}
         <div key={`body`} id='___gatsby' dangerouslySetInnerHTML={{ __html: props.body }} />
