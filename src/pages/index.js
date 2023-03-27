c485f9455456b4f89e5ee6591ee87d8b1eee6c88
yeyuxin <yuxin.ye@take2.health>
2023-03-27 10:12:48 +0800

    解决document不存在的问题

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,22 +2,23 @@ import * as React from "react"
 import Layout from '../components/layout'
 
 const IndexPage = () => {
+  const isSSR = typeof document === "undefined"
   return (
-    <Layout pageTitle="Home Page">
+    !isSSR && <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial.</p>
       <a 
         class="twitter-share-button"
         target="_blank"
         id="hhhhh"
         href="https://www.youtube.com/"
-        pageName={document.title}
+        pageName={document?.title}
         buttonId="RW_HP_Top_Banner_Athlete_EHEALTH"
         name="立即预约"
         onClick={()=>{
           window.dataLayer = window.dataLayer || [];
           window.dataLayer.push({
           event: "share",
-          page_name: document.title,
+          page_name: document?.title,
           button_name: "buttonName",
           button_ID: "button2222"
           })}}
@@ -29,7 +30,7 @@ const IndexPage = () => {
         window.dataLayer = window.dataLayer || [];
         window.dataLayer.push({
         event: "click",
-        page_name: document.title,
+        page_name: document?.title,
         button_name: "buttonName",
         button_ID: "button2222"
         })}}>button</button>
