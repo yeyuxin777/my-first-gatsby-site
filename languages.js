7aee0893a20e6879ede01eed6e59df27ff439307
yeyuxin <yuxin.ye@take2.health>
2023-04-13 17:59:25 +0800

    no message

---
+++ languages.js
@@ -0,0 +1,24 @@
+module.exports = {
+  languages: ['zh-HK', 'zh-CN', 'en'],
+  languagePrefixes: {
+    'zh-HK': 'Hk',
+    'zh-CN': 'Cn',
+    en: 'En',
+  },
+  defaultLanguage: `zh-HK`,
+  languageLabels: [
+    {
+      shortLabel: '繁',
+      label: '繁體中文',
+      lang: 'zh-HK',
+      id: 'ECP_ Language_TCHI',
+    },
+    {
+      shortLabel: '简',
+      label: '简体中文',
+      lang: 'zh-CN',
+      id: 'ECP_ Language_SCHI',
+    },
+    { shortLabel: 'EN', label: 'English', lang: 'en', id: 'ECP_ Language_EN' },
+  ],
+}
